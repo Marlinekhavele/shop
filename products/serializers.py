23f7a11d40129059ed8aa1c8eb284fb4bd725788
MarlineKhavele <marlinekhavele6@gmail.com>
2021-03-22 20:28:33 +0300

    added orders and AT

--- products/serializers.py
+++ products/serializers.py
@@ -2,8 +2,8 @@ from rest_framework.serializers import ModelSerializer
 from products.models import Product
 
 
-class ProductSizeSerializer(ModelSerializer):
-    class Meta:
-        model = ProductSize
-        fields = ['id','title','active']
+class ProductSerializer(ModelSerializer):
 
+    class Meta:
+        fields = '__all__' 
+        model = Product
--- store/urls.py
+++ store/urls.py
@@ -20,8 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
-    # path('api/v1/', include('products.urls')),
-    # path('api/v1/', include('orders.urls')),
+    path('api/v1/', include('products.urls')),
+    path('api/v1/', include('orders.urls')),
 
     
 ]
