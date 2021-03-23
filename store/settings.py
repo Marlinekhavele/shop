cada2768edb9be2dc6e62056dad334af8ea5b94f
MarlineKhavele <marlinekhavele6@gmail.com>
2021-03-24 02:06:00 +0300

    make it better

--- store/settings.py
+++ store/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
+    # 'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
