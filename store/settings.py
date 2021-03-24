90c9a7a36240237d47e9251d6ef7742860f04b9c
MarlineKhavele <marlinekhavele6@gmail.com>
2021-03-24 11:24:03 +0300

    fixes

--- store/settings.py
+++ store/settings.py
@@ -142,8 +142,8 @@ STATICFILES_DIRS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.permissions.IsAuthenticated',
 
     ),
     'DEFAULT_FILTER_BACKENDS': (
